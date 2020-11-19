0ea90de308c6d1a4198b632b3bec3017feea884d
Tănase Matei <tanasemat@gmail.com>
2020-11-19 14:10:07 +0200

    Statistics endpoints matei&maria (#21)
    * statistics endpoint added

--- src/transactions/transaction.gateway.ts
+++ src/transactions/transaction.gateway.ts
@@ -1,6 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { Database } from 'src/system/database';
+import { ConfigProvider } from 'src/system/ConfigProvider';
 
 @Injectable()
 export class TransactionGateway extends Database {
+	transactionTable: string;
+	bankAccountTable: string;
+	constructor(configProvider: ConfigProvider) {
+		super(configProvider);
+		this.transactionTable = 'transactions';
+		this.bankAccountTable = 'bank_accounts';
+	}
+	getLastTransactions(days: number, userId: number): Promise<any> {
+		const sql = `
+		SELECT * FROM ${this.transactionTable} as tr
+		LEFT JOIN ${this.bankAccountTable} as bank
+		ON tr.account_id = bank.id
+		WHERE (tr.date_time > NOW() - INTERVAL ? DAY) AND bank.user_id = ?
+		LIMIT 5;
+		`;
+		return this.query({
+			sql,
+			values: [days, userId],
+		});
+	}
+	getLastTransactionsSum(days: number, userId: number): Promise<any> {
+		const sql = `
+		SELECT coalesce(SUM(amount), 0) as amount FROM ${this.transactionTable} as tr
+		LEFT JOIN ${this.bankAccountTable} as bank
+		ON tr.account_id = bank.id
+		WHERE (tr.date_time > NOW() - INTERVAL ? DAY) AND bank.user_id = ?;
+		`;
+		return this.query({
+			sql,
+			values: [days, userId],
+		});
+	}
 }
--- src/transactions/transaction.service.ts
+++ src/transactions/transaction.service.ts
@@ -1,10 +1,22 @@
+import { ITransaction } from './models/Transactions';
 import { Injectable } from '@nestjs/common';
 import { ConfigProvider } from 'src/system/ConfigProvider';
 import { TransactionGateway } from './transaction.gateway';
 
 @Injectable()
 export class TransactionService {
-	constructor(private gateway: TransactionGateway, private configProvider: ConfigProvider) {
+
+	constructor(private gateway: TransactionGateway, private configProvider: ConfigProvider) {}
+
+	async lastTransactions(days: number, userId: number): Promise<ITransaction[]> {
+		const result = await this.gateway.getLastTransactions(days,userId);
+		if (!result) {
+			return [];
+		}
+		return result;
+	}
+	async lastTransactionsAmount(days: number, userId: number): Promise<number>  {
+		const [result] = await this.gateway.getLastTransactionsSum(days,userId);
+		return result.amount;
 	}
 }
-
