c50e21f8da45cbc2396a071119b86348cb919dbb
Andrei Galanton <72964493+AndreiGalanton@users.noreply.github.com>
2020-11-19 12:22:44 +0200

    update1

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,6 +5,7 @@ import { UserService } from './user.service';
 //import { BcryptStrategy } from './bcrypt.strategy';
 import { AuthModule } from 'src/auth/auth.module';
 import { MailerModule } from 'src/mailer/mailer.module';
+//de importat modulul de mailer
 
 @Module({
 	providers: [UserGateway, UserService],
